ed7df24140be0822d126707d08ee0350e942b7e1
GabrielRodrigues07 <grodrigues256@gmail.com>
2022-12-18 11:37:31 -0300

    implementando Circuit breaker e fallback com Resilience 4j

--- src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -76,4 +76,15 @@ public class PagamentoService {
         pagamentoRepository.save(pagamento.get());
         pedido.atualizaPagamento(pagamento.get().getPedidoId());
     }
+
+    public void alterarStatus(Long id) {
+        Optional<Pagamento> pagamento = pagamentoRepository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO_SEM_INTEGRACAO);
+        pagamentoRepository.save(pagamento.get());
+    }
 }
